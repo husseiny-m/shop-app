dc6366fe056e445b743731a99e3b3e20d8f27143
Husseiny <husseiny.mohammed@gmail.com>
2019-12-25 10:34:57 +0200

    adding collection item component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 // TODO: all this calls cause (ie map, filter, ...) performance issue as it called at every render, we will visit this later.
 const CollectionPreview = ({ title, items }) => (
@@ -10,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((_item, index) => index < 4)
-        .map((item) => (
-          <div key={item.id}> {item.name}</div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>

310bcf5729b3f181e3d3c66b1a0f79c8f3a01612
Husseiny <husseiny.mohammed@gmail.com>
2019-12-25 11:00:53 +0200

    added header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
 function App() {
   return (
-    <div >
+    <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        </Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }

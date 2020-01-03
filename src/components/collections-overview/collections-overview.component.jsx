96e0b8f941db201d1de0022f3764fe4866668f91
Husseiny <husseiny.mohammed@gmail.com>
2020-01-03 08:05:56 +0200

    move the home page component to collections-overview component

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { selectShopCollections } from '../../redux/shop/shope.selectors';
+
+const CollectionsOverview = ({ collections }) => {
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections
+});
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { selectShopCollections } from '../../redux/shop/shope.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopePage = ({ collections }) => {
-  return (
-    <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-  );
+import './shop.styles.scss';
+
+const ShopePage = () => {
+  return <CollectionsOverview />;
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-});
-export default connect(mapStateToProps)(ShopePage);
+export default ShopePage;
